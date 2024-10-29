74cc6b79c6e0122de6f3fde38e5162cd31bad9fe
Alvarocardenasg <acardenas1132@ceuandalucia.es>
2024-10-29 16:55:21 +0100

    cambio

--- Proyecto3_3/src/boletin/Clase3_3.java
+++ Proyecto3_3/src/boletin/Clase3_3.java
@@ -8,7 +8,7 @@ public class Clase3_3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Practica 3_3 Alvaro Cardenas Granja");
+		System.out.println("Canela En Rama");
 		
 	}
 
